21d1719072695d49615af1b79cdf903e5f5dc4c2
sivaram <sivaram3710@gmail.com>
2024-11-12 14:25:05 +0530

    Modified root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Lato } from "next/font/google";
+import RootHeader from "@/components/root-header";
+import RootFooter from "@/components/root-footer";
 
 export const metadata = {
 	title: "Sivaram Portfolio",
@@ -15,7 +17,11 @@ const lato = Lato({
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={`${lato} antialiased`}>{children}</body>
+			<body className={`${lato} bg-gray-800 text-gray-200 antialiased`}>
+				<RootHeader />
+				{children}
+				<RootFooter />
+			</body>
 		</html>
 	);
 }
