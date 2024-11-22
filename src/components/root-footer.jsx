ac55f8e69bf4fb614a33d4da572d276d2607a7aa
sivaram <sivaram3710@gmail.com>
2024-11-22 16:14:51 +0530

    added footer to the layout

--- src/components/root-footer.jsx
+++ src/components/root-footer.jsx
@@ -4,38 +4,50 @@ import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 export default function RootFooter() {
 	return (
-		<footer className="px-4 md:px-12 border-b-8 md:border-b-[12px] border-primary">
-			<div className="px-3 pb-3 md:px-4 md:pb-0 border-t border-gray-500" />
-			<ul className="space-y-4 md:flex md:justify-between md:items-center">
-				<li>
-					<h4 className="text-sm md:text-xl font-semibold">Phone</h4>
-					<p className="text-xs md:text-sm font-thin text-gray-400">+91 8940671901</p>
-				</li>
-				<li>
-					<h4 className="text-sm md:text-xl font-semibold">Email</h4>
-					<p className="text-xs md:text-sm font-thin text-gray-400">sivaram3710@gmail.com</p>
-				</li>
-				<li>
-					<h4 className="text-sm md:text-xl font-semibold">Follow Me</h4>
-					<div className="mt-1 py-1 flex gap-4">
-						<Link href="https://www.linkedin.com/in/sivaramvajravelu" target="_blank">
-							<BsLinkedin className="hover:text-primary" size={20} />
+		<footer>
+			<div className="px-4 md:px-12">
+				<div className="px-3 pb-3 md:px-4 md:pb-0 border-t border-primary" />
+				<ul className="space-y-4 md:flex md:justify-between md:items-center">
+					<li>
+						<h4 className="text-sm md:text-xl font-semibold">Phone</h4>
+						<Link
+							href="tel:+918940671901"
+							className="text-xs md:text-sm font-thin text-gray-400 hover:text-blue-400"
+						>
+							+91 89406 71901
 						</Link>
-						<Link href="#" target="_blank">
-							<BsGithub className="hover:text-primary" size={20} />
+					</li>
+					<li>
+						<h4 className="text-sm md:text-xl font-semibold">Email</h4>
+						<Link
+							href="mailto:sivaram3710@gmail.com"
+							className="text-xs md:text-sm font-thin text-gray-400 hover:text-blue-400"
+						>
+							sivaram3710@gmail.com
 						</Link>
-						<Link href="#" target="_blank">
-							<BsInstagram className="hover:text-primary" size={20} />
-						</Link>
-						<Link href="#" target="_blank">
-							<AiOutlineX className="hover:text-primary" size={20} />
-						</Link>
-					</div>
-				</li>
-			</ul>
-			<p className="pt-4 pb-1 md:pt-8 flex justify-center text-gray-400 text-xs md:text-sm">
-				&copy; 2024 By Sivaram.
-			</p>
+					</li>
+					<li>
+						<h4 className="text-sm md:text-xl font-semibold">Follow Me</h4>
+						<div className="mt-1 py-1 flex gap-4">
+							<Link href="https://www.linkedin.com/in/sivaramvajravelu" target="_blank">
+								<BsLinkedin className="text-gray-400 hover:text-blue-400" size={20} />
+							</Link>
+							<Link href="#" target="_blank">
+								<BsGithub className="text-gray-400 hover:text-blue-400" size={20} />
+							</Link>
+							<Link href="#" target="_blank">
+								<BsInstagram className="text-gray-400 hover:text-blue-400" size={20} />
+							</Link>
+							<Link href="#" target="_blank">
+								<AiOutlineX className="text-gray-400 hover:text-blue-400" size={20} />
+							</Link>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div className="mt-3 py-1 flex justify-center items-start text-gray-200 bg-primary text-[11px] md:text-xs">
+				<p>&copy; {new Date().getFullYear()} By Sivaram.</p>
+			</div>
 		</footer>
 	);
 }
