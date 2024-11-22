c0debbc7caef5b3c709dfc53d0e6476d26e138cf
sivaram <sivaram3710@gmail.com>
2024-11-22 16:14:14 +0530

    added hover to my name

--- src/components/root-header.jsx
+++ src/components/root-header.jsx
@@ -9,7 +9,9 @@ export default function RootHeader() {
 					<li>
 						<Link href="/" className="space-x-1 md:space-x-2">
 							<span className="inline-block h-3.5 w-3.5 rounded-full md:h-5 md:w-5 bg-primary" />
-							<span className="font-bold text-lg md:text-2xl">Sivaram V</span>
+							<span className="font-bold text-lg md:text-2xl hover:text-gray-300">
+								Sivaram V
+							</span>
 						</Link>
 					</li>
 					<li className="font-thin text-blue-300 text-xs md:text-sm">Web Developer</li>
