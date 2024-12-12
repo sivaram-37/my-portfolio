51f571123da853138f986d0facdb90bdfc969bb9
sivaram <sivaram3710@gmail.com>
2024-12-12 17:56:05 +0530

    changed sub title color to blue

--- src/components/resume/resume-subtitle.jsx
+++ src/components/resume/resume-subtitle.jsx
@@ -1,5 +1,5 @@
 export default function ResumeSubtitle({ subTitle }) {
 	return (
-		<h1 className="ml-4 md:ml-8 mt-3 md:mt-5 text-sm md:text-lg text-gray-700">{subTitle}</h1>
+		<h1 className="ml-4 md:ml-8 mt-3 md:mt-5 text-sm md:text-lg text-blue-500">{subTitle}</h1>
 	);
 }
